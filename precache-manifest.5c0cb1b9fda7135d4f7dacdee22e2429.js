e3cdeb0b5b2580f0086e38cf8e5b26af4707f98e
markke78 <56062146+markke78@users.noreply.github.com>
2020-01-31 15:30:07 -0700

    Updates

---
+++ precache-manifest.5c0cb1b9fda7135d4f7dacdee22e2429.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c4f22353f5c8d8e72eadd22c0b4259",
+    "url": "/markke78/mobileReactApp.git/index.html"
+  },
+  {
+    "revision": "c795402e92697f5879f8",
+    "url": "/markke78/mobileReactApp.git/static/css/main.e54d6f5e.chunk.css"
+  },
+  {
+    "revision": "c7a852449117361ec645",
+    "url": "/markke78/mobileReactApp.git/static/js/2.9744b185.chunk.js"
+  },
+  {
+    "revision": "c795402e92697f5879f8",
+    "url": "/markke78/mobileReactApp.git/static/js/main.a3090ea0.chunk.js"
+  },
+  {
+    "revision": "d8a3fba14385286e0123",
+    "url": "/markke78/mobileReactApp.git/static/js/runtime-main.dd60d60e.js"
+  },
+  {
+    "revision": "7d669b4ef930173903c34e10cecf14af",
+    "url": "/markke78/mobileReactApp.git/static/media/Fire.7d669b4e.svg"
+  },
+  {
+    "revision": "2d3a1701793815ab2a92b836203b9cf9",
+    "url": "/markke78/mobileReactApp.git/static/media/Goomba.2d3a1701.svg"
+  },
+  {
+    "revision": "3e020c2df63727441f025a0c37ae601f",
+    "url": "/markke78/mobileReactApp.git/static/media/Green.3e020c2d.svg"
+  },
+  {
+    "revision": "7a413553d4186e47688a46bfe83a23bc",
+    "url": "/markke78/mobileReactApp.git/static/media/LuigiF.7a413553.svg"
+  },
+  {
+    "revision": "4fbe4c5bcd16f542faf625452d7ee5e2",
+    "url": "/markke78/mobileReactApp.git/static/media/MarioF.4fbe4c5b.svg"
+  },
+  {
+    "revision": "394ecfceec7c124edf9c9232293ef11c",
+    "url": "/markke78/mobileReactApp.git/static/media/Peach.394ecfce.svg"
+  },
+  {
+    "revision": "3edb92edda816043c750e0aeb9a82355",
+    "url": "/markke78/mobileReactApp.git/static/media/Red.3edb92ed.svg"
+  },
+  {
+    "revision": "adcc5533646ab6df579a3479a78e8159",
+    "url": "/markke78/mobileReactApp.git/static/media/Toad.adcc5533.svg"
+  },
+  {
+    "revision": "03b7fc26fe494b4e4215d43cef08afa6",
+    "url": "/markke78/mobileReactApp.git/static/media/context.03b7fc26.svg"
+  },
+  {
+    "revision": "6315d98eb5114501cb83b930c8101940",
+    "url": "/markke78/mobileReactApp.git/static/media/icon.6315d98e.svg"
+  },
+  {
+    "revision": "5d9371e42ea6a1bfecea03cd18681b6a",
+    "url": "/markke78/mobileReactApp.git/static/media/link.5d9371e4.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/markke78/mobileReactApp.git/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "12492fa579c872d3a8b18fd9d5b92ece",
+    "url": "/markke78/mobileReactApp.git/static/media/luigiX.12492fa5.png"
+  },
+  {
+    "revision": "c92192a3b4d958659fb0e1aa1f54ebb5",
+    "url": "/markke78/mobileReactApp.git/static/media/marioO.c92192a3.png"
+  },
+  {
+    "revision": "643200dd18969ab1899e4955c84a1f67",
+    "url": "/markke78/mobileReactApp.git/static/media/money.643200dd.svg"
+  },
+  {
+    "revision": "913923a8824cfef9ffde332c366b0644",
+    "url": "/markke78/mobileReactApp.git/static/media/smb.913923a8.png"
+  },
+  {
+    "revision": "963d81f5e99c7058c36d69e04d27da16",
+    "url": "/markke78/mobileReactApp.git/static/media/smb1.963d81f5.png"
+  },
+  {
+    "revision": "3dceb8cbe16b295f5b801974396632e2",
+    "url": "/markke78/mobileReactApp.git/static/media/star.3dceb8cb.svg"
+  },
+  {
+    "revision": "740827177b0f97c0627d794b9145335f",
+    "url": "/markke78/mobileReactApp.git/static/media/tictactoe.74082717.svg"
+  },
+  {
+    "revision": "d4b454f4fb83890d6cbc077cfcf92f7d",
+    "url": "/markke78/mobileReactApp.git/static/media/tictactoebg1.d4b454f4.png"
+  }
+]);
